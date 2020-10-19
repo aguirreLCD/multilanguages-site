369a0cdfbf4d20366a6029ae0d25063833e55dd7
aguirreLCD <lilidaminelli@gmail.com>
2020-10-19 19:19:55 +0200

    pt-en-es-experiences

--- src/Components/Homepage/index.js
+++ src/Components/Homepage/index.js
@@ -10,11 +10,13 @@ export default (props) => {
         <h3>{t("homeDescription")}</h3>
         {/* <p>{t("homeFreelance")}</p> */}
 
+        {/* <div id="presentation"> */}
         {t("homeFreelance")
           .split("/n")
           .map((text, i) => (
             <p dangerouslySetInnerHTML={{ __html: text }} key={i}></p>
           ))}
+        {/* </div> */}
 
         <div>
           <Link to="/bio" className="btn btn-primary">
